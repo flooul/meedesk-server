3a830f7b315cce15eed8c90c60cafd0ab6f80414
yiwen <18820909494@163.com>
2020-08-11 16:55:52 +0800

    11

--- install-services.sh
+++ install-services.sh
@@ -17,11 +17,11 @@ docker version
 docker login -u 18820909494 -p docker132879
 echo "创建并启动可以可视化工具服务"
 #创建网络
-docker network create backend
+#docker network create backend
 docker run -d -p 9000:9000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /public:/public --name prtainer docker.io/portainer/portainer
-开始安装MySQL服务器
 echo "创建并启动mysql数据库服务"
-docker run -d --name mysql57 --net=backend -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pts123456. --restart=always -v /opt/meeting-server/mysql/data:/var/lib/mysql -v /opt/meeting-server/mysql/config/mysqld.cnf:/etc/mysql/conf.d/mysqld.cnf -v /opt/meeting-server/mysql/sql:/docker-entrypoint-initdb.d hub.c.163.com/library/mysql:5.7
+#--net=backend
+docker run -d --name mysql57  -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pts123456. --restart=always -v /opt/meeting-server/mysql/data:/var/lib/mysql -v /opt/meeting-server/mysql/config/mysqld.cnf:/etc/mysql/conf.d/mysqld.cnf -v /opt/meeting-server/mysql/sql:/docker-entrypoint-initdb.d hub.c.163.com/library/mysql:5.7
 echo "创建并启动nginx-rtmp服务"
 docker run --name nginx-rtmp -p 1935:1935 -p 8899:80 --restart=always -d -it 18820909494/yiwen:nginx-rtmp
 echo "创建并启动onlyoffice文件服务"
